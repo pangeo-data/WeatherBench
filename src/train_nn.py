3ec1e62831234e9431feebc444726f50f306f445
Stephan Rasp <raspstephan@gmail.com>
2019-12-12 13:18:43 +0100

    Further fixes for NN runscript

--- src/train_nn.py
+++ src/train_nn.py
@@ -16,6 +16,7 @@ def limit_mem():
 
 class DataGenerator(keras.utils.Sequence):
     """https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly"""
+
     def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True, mean=None, std=None):
 
         self.ds = ds
@@ -31,16 +32,19 @@ class DataGenerator(keras.utils.Sequence):
                 data.append(ds[var].sel(level=levels))
             except ValueError:
                 data.append(ds[var].expand_dims({'level': generic_level}, 1))
+
         self.data = xr.concat(data, 'level').transpose('time', 'lat', 'lon', 'level')
-        if load: print('Loading data into RAM'); self.data.load()
-        self.mean = self.data.mean(('time', 'lat', 'lon')) if mean is None else mean
-        self.std = self.data.std('time').mean(('lat', 'lon')) if std is None else std
+        self.mean = self.data.mean(('time', 'lat', 'lon')).compute() if mean is None else mean
+        self.std = self.data.std('time').mean(('lat', 'lon')).compute() if std is None else std
         # Normalize
         self.data = (self.data - self.mean) / self.std
         self.n_samples = self.data.isel(time=slice(0, -lead_time)).shape[0]
         self.valid_time = self.data.isel(time=slice(lead_time, None)).time
 
         self.on_epoch_end()
+
+        # For some weird reason calling .load() earlier messes up the mean and std computations
+        if load: print('Loading data into RAM'); self.data.load()
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -58,7 +62,6 @@ class DataGenerator(keras.utils.Sequence):
         self.idxs = np.arange(self.n_samples)
         if self.shuffle == True:
             np.random.shuffle(self.idxs)
-
 
 class PeriodicConv2D(tf.keras.layers.Conv2D):
     """Convolution with periodic padding in second spatial dimension (lon)"""
@@ -160,9 +163,9 @@ def main(datadir, vars, filters, kernels, lr, activation, dr, batch_size, patien
     dic = {var: None for var in vars}
     dg_train = DataGenerator(ds_train, dic, lead_time, batch_size=batch_size)
     dg_valid = DataGenerator(ds_valid, dic, lead_time, batch_size=batch_size, mean=dg_train.mean,
-                             std=dg_train.std)
+                             std=dg_train.std, shuffle=False)
     dg_test =  DataGenerator(ds_test, dic, lead_time, batch_size=batch_size, mean=dg_train.mean,
-                             std=dg_train.std)
+                             std=dg_train.std, shuffle=False)
     print(f'Mean = {dg_train.mean}; Std = {dg_train.std}')
 
     # Build model
