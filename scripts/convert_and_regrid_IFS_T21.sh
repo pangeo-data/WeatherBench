1501cae71ff020cf83e7fe0b13f2a827399d6cb9
Stephan Rasp <raspstephan@gmail.com>
2020-02-07 07:54:36 +0100

    Added T63 evaluation

--- scripts/convert_and_regrid_IFS_T21.sh
+++ scripts/convert_and_regrid_IFS_T21.sh
@@ -3,8 +3,8 @@ for file in /media/rasp/Elements/weather-benchmark/IFS_T21/raw/*; do
   cdo -f nc copy "$file" "${file%.grib}.nc"
 done
 
-# Regrid to 5.625 degrees
-python ../src/regrid.py \
---input_fns /media/rasp/Elements/weather-benchmark/IFS_T42/raw/output_42_pl_2.8125*.nc \
---output_dir /media/rasp/Elements/weather-benchmark/IFS_T42/ \
---ddeg_out 5.625
+## Regrid to 5.625 degrees
+#python ../src/regrid.py \
+#--input_fns /media/rasp/Elements/weather-benchmark/IFS_T42/raw/output_42_pl_2.8125*.nc \
+#--output_dir /media/rasp/Elements/weather-benchmark/IFS_T42/ \
+#--ddeg_out 5.625
